36122a9f5373c43b07eb4b6aa153df9eefe2d577
Lucifer Chase <luciferchase@noreply.github.com>
2021-04-26 22:05:51 +0530

    Initial Commit

---
+++ bot/cogs/botstatus/botstatus.py
@@ -0,0 +1,222 @@
+import discord
+from discord.ext import tasks
+import asyncio
+from typing import Optional
+
+class Botstatus(commands.Cog):
+    """Botstatus"""
+
+    def __init__(self, bot):
+        self.ready = False
+        self.bot = bot
+        standard = {"status": (None, None, None)}
+        self.ready = True
+        self.start_task: Optional[asyncio.Task] = None
+        self._update_task.start()
+
+    def init(self):
+        self.start_task = asyncio.create_task(self.fromconf())
+
+    def cog_unload(self):
+        self._update_task.cancel()
+        if self.start_task:
+            self.start_task.cancel()
+
+    @tasks.loop(minutes=10)
+    async def _update_task(self):
+        await self.fromconf()
+
+    async def setfunc(self, sType, status, text):
+        # This will get removed in future versions and is to ensure config backwards-compatibility
+        if sType == "game":
+            sType = "playing"
+        t = getattr(discord.ActivityType, sType, False)
+        s = getattr(discord.Status, status, False)
+        if not (t and s):
+            return
+        activity = discord.Activity(name = text, type = t)
+        await self.bot.change_presence(status = s, activity = activity)
+
+    async def fromconf(self):
+        await self.bot.wait_until_ready()
+        value = await self.config.status()
+        if value[0] and value[1] and value[2]:
+            await self.setfunc(value[0], value[1], value[2])
+
+    @commands.group()
+    @checks.is_owner()
+    async def botstatus(self, ctx):
+        """Set a status that doesn't dissappear on reboot.
+        Usage: [p]botstatus <type> <status> <text>"""
+        pass
+
+    @botstatus.group(name = "playing", aliases = ["game"])
+    async def game(self, ctx):
+        """Usage: [p]botstatus playing <status> <text>"""
+        pass
+
+    @game.command(name = "online")
+    async def g_online(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("playing", "online", text))
+            await self.setfunc("playing", "online", text)
+            await ctx.send(_("Status set to ``Online | Playing {text}``").format(text=text))
+
+    @game.command(name = "idle")
+    async def g_idle(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("playing", "idle", text))
+            await self.setfunc("playing", "idle", text)
+            await ctx.send(_("Status set to ``Idle | Playing {text}``").format(text=text))
+
+    @game.command(name = "dnd")
+    async def g_dnd(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("playing", "dnd", text))
+            await self.setfunc("playing", "dnd", text)
+            await ctx.send(_("Status set to ``DND | Playing {text}``").format(text=text))
+
+    @game.command(name="offline")
+    async def g_offline(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("playing", "offline", text))
+            await self.setfunc("playing", "offline", text)
+            await ctx.send(_("Status set to ``Offline | Playing {text}``").format(text=text))
+
+    @botstatus.group()
+    async def listening(self, ctx):
+        """Usage: [p]botstatus listening <status> <text>"""
+        pass
+
+    @listening.command(name="online")
+    async def l_online(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("listening", "online", text))
+            await self.setfunc("listening", "online", text)
+            await ctx.send(_("Status set to ``Online | Listening to {text}``").format(text=text))
+
+    @listening.command(name="idle")
+    async def l_idle(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("listening", "idle", text))
+            await self.setfunc("listening", "idle", text)
+            await ctx.send(_("Status set to ``Idle | Listening to {text}``").format(text=text))
+
+    @listening.command(name="dnd")
+    async def l_dnd(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("listening", "dnd", text))
+            await self.setfunc("listening", "dnd", text)
+            await ctx.send(_("Status set to ``DND | Listening to {text}``").format(text=text))
+
+    @listening.command(name="offline")
+    async def l_offline(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("listening", "offline", text))
+            await self.setfunc("listening", "offline", text)
+            await ctx.send(_("Status set to ``Offline | Listening to {text}``").format(text=text))
+
+    @botstatus.group()
+    async def watching(self, ctx):
+        """Usage: [p]botstatus watching <status> <text>"""
+        pass
+
+    @watching.command(name="online")
+    async def w_online(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("watching", "online", text))
+            await self.setfunc("watching", "online", text)
+            await ctx.send(_("Status set to ``Online | Watching {text}``").format(text=text))
+
+    @watching.command(name="idle")
+    async def w_idle(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("watching", "idle", text))
+            await self.setfunc("watching", "idle", text)
+            await ctx.send(_("Status set to ``Idle | Watching {text}``").format(text=text))
+
+    @watching.command(name="dnd")
+    async def w_dnd(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("watching", "dnd", text))
+            await self.setfunc("watching", "dnd", text)
+            await ctx.send(_("Status set to ``DND | Watching {text}``").format(text=text))
+
+    @watching.command(name="offline")
+    async def w_offline(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("watching", "offline", text))
+            await self.setfunc("watching", "offline", text)
+            await ctx.send(_("Status set to ``Offline | Watching {text}``").format(text=text))
+
+    @botstatus.group()
+    async def competing(self, ctx):
+        """Set a competing status"""
+        pass
+
+    @competing.command(name="online")
+    async def c_online(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("competing", "online", text))
+            await self.setfunc("competing", "online", text)
+            await ctx.send(_("Status set to ``Online | Competing {text}``").format(text=text))
+ 
+    @competing.command(name="idle")
+    async def c_idle(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("competing", "idle", text))
+            await self.setfunc("competing", "idle", text)
+            await ctx.send(_("Status set to ``Idle | Competing {text}``").format(text=text))
+
+    @competing.command(name="dnd")
+    async def c_dnd(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("competing", "dnd", text))
+            await self.setfunc("competing", "dnd", text)
+            await ctx.send(_("Status set to ``DND | Competing {text}``").format(text=text))
+
+    @competing.command(name="offline")
+    async def c_offline(self, ctx, *, text: str):
+        if len(text) > 128:
+            await ctx.send(_("The chracter limit for status messages is 128."))
+        else:
+            await self.config.status.set(("competing", "offline", text))
+            await self.setfunc("competing", "offline", text)
+            await ctx.send(_("Status set to ``Offline | Competing {text}``").format(text=text))
+
+    @botstatus.command()
+    async def clear(self, ctx):
+        """Clear the saved botstatus and disable auto-setting on reboot."""
+        await self.config.status.set((None, None, None))
+        await ctx.send(_("Saved botstatus has been cleared."))
+        await self.bot.change_presence(status=discord.Status.online, activity=None)
--- bot/main.py
+++ bot/main.py
@@ -8,22 +8,27 @@ import logging
 # Configure the bot
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = commands.Bot(command_prefix = "luci ")
+
 logging.basicConfig(level = logging.INFO)
 
-# Commands
+# Register Cogs
+bot.add_cog(Botstatus(bot))
+
+# Core Commands
 @bot.event
 async def on_ready():
-	log = logging.getLogger("on_ready")
 	try:
 		await bot.change_presence(
 			status = discord.Status.idle, 
-			activity = discord.CustomActivity(name = "Always with you")
+			activity = discord.Activity(
+					type = discord.ActivityType.listening,
+					name = "your heartbeats"
+				)
 		)
+		print("Activity set successfully")
 	except:
-		log.error("Cannot set activity")
 		print("Cannot set activity")
 	print("Connected to discord")
-
 
 @bot.command()
 async def ping(ctx) :
