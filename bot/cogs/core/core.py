2535356ce3fe2efe7698f4b899aed7d8b0213afe
Lucifer Chase <luciferchase@noreply.github.com>
2021-04-27 19:25:22 +0530

    Remove scheduler

--- bot/cogs/core/core.py
+++ bot/cogs/core/core.py
@@ -1,11 +1,6 @@
 import discord
 from discord.ext import commands
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-
-from cogs.meme.meme import Meme
-
 class Core(commands.Cog):
 	"""Core commands"""	
 	def __init__(self, bot):
@@ -26,14 +21,6 @@ class Core(commands.Cog):
 			print("Cannot set activity")
 		
 		print("Connected to discord")
-
-		#initializing scheduler
-		scheduler = AsyncIOScheduler()
-
-		scheduler.add_job(Meme.meme, CronTrigger(second = "0, 30, 57")) 
-
-		#starting the scheduler
-		scheduler.start()
 
 	@commands.Cog.listener()
 	async def on_member_join(self, member):
