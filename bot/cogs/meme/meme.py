4f7b2b502d3166125b6b75371cc8bb70f04778f1
Lucifer Chase <luciferchase@noreply.github.com>
2021-04-28 15:57:19 +0530

    Try scheduling memes

--- bot/cogs/meme/meme.py
+++ bot/cogs/meme/meme.py
@@ -5,22 +5,24 @@ import requests
 import json
 import random
 import logging
+from datetime import datetime
 
 
 class Meme(commands.Cog):
 	""" Get memes.
 	"""
 	
-	def __init__(self):
+	def __init__(self, bot):
+		self.bot = bot
+
 		self.log = logging.getLogger("red.cogsbylucifer.photo")
 		self.dog_api = "https://api.thedogapi.com/v1/images/search"
+
+		# Starting the scheduled running of meme command
+		self.scheduler.start()
 	
-	@commands.command()
-	async def meme(self, ctx, endpoint = ""):
-		""" Get a meme from reddit. 
-			To Get a meme from a specific subreddit, append its name after the command
-			For eg. `luci meme wholesomememes`
-		"""
+	def meme_code(self, endpoint = ""):
+		
 		api = "https://meme-api.herokuapp.com/gimme"
 		if (endpoint != ""):
 			response = requests.get(api + "/" + endpoint).json()
@@ -43,8 +45,7 @@ class Meme(commands.Cog):
 				value = "Try again maybe? Anyway here is a cute doggo ❤"
 			)
 			embed.set_image(url = response_dog["url"])
-			await ctx.send(embed = embed)
-			return
+			return embed
 		
 		elif (response["nsfw"]):
 			response = requests.get(self.api)
@@ -56,4 +57,29 @@ class Meme(commands.Cog):
 		)
 		embed.set_image(url = response["url"])
 		embed.set_footer(text = f'👍 {response["ups"]}')
+		return embed
+
+	@commands.command()
+	async def meme(self, ctx, endpoint = ""):
+		""" Get a meme from reddit. 
+			To Get a meme from a specific subreddit, append its name after the command
+			For eg. `luci meme wholesomememes`
+		"""
+		embed = meme_code(endpoint)
 		await ctx.send(embed = embed)
+
+	# Schedule meme to run every 30 seconds
+	@tasks.loop(seconds = 30, count = 5)
+	async def scheduler(self, ctx):
+		embed = meme_code()
+		await ctx.send(embed = embed)
+
+	# Wait until the bot is ready
+	@scheduler.before_loop
+	async def before_scheduler(self):
+		print("Waiting for the bot to get ready...")
+		await self.bot.wait_until_ready()
+
+	@scheduler.after_loop
+	async def after_scheduler(self):
+		print(f"Executed meme at {datetime.now()}")
