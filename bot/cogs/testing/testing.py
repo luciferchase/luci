18cfca336173e8c290254f29b562e7135c3028a6
Lucifer Chase <luciferchase@noreply.github.com>
2021-04-29 13:12:17 +0530

    Fix minor issue

--- bot/cogs/testing/testing.py
+++ bot/cogs/testing/testing.py
@@ -15,6 +15,13 @@ class Testing(commands.Cog):
 		dbcon = psycopg2.connect(DATABASE_URL, sslmode = "require")
 		cursor = dbcon.cursor()
 
+		cursor.execute("DELETE FROM CONFIG")
+
+		query = """INSERT INTO CONFIG VALUES
+				(1, '2021-04-28', 1254079, 0, 0)"""
+		cursor.execute(query)
+		dbcon.commit()
+
 		query = """INSERT INTO STANDINGS VALUES
 				(707557256220115035, 30),
 				(650661454000947210, 30)"""
