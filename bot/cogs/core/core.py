b73c3b84984dc8bf1e53155c11ef41f271e67fbd
Lucifer Chase <luciferchase@noreply.github.com>
2021-04-30 17:42:20 +0530

    Minor fix

--- bot/cogs/core/core.py
+++ bot/cogs/core/core.py
@@ -154,104 +154,104 @@ class Help(commands.Cog):
 		owner = 707557256220115035
 		owner_name = "luciferchase#6310"
 
-			# checks if cog parameter was given
-			# if not: sending all modules and commands not associated with a cog
-			if not input:
-				# checks if owner is on this server - used to 'tag' owner
-				try:
-					owner = ctx.guild.get_member(owner).mention
+		# checks if cog parameter was given
+		# if not: sending all modules and commands not associated with a cog
+		if not input:
+			# checks if owner is on this server - used to 'tag' owner
+			try:
+				owner = ctx.guild.get_member(owner).mention
 
-				except AttributeError as error:
-					owner = owner
+			except AttributeError as error:
+				owner = owner
 
-				# starting to build embed
-				embed = discord.Embed(
-					title = "Commands and modules", 
-					color = discord.Color.blue(),
-					description = f"Use `{prefix}help <module>` to gain more information about that module 😁"
-				)
+			# starting to build embed
+			embed = discord.Embed(
+				title = "Commands and modules", 
+				color = discord.Color.blue(),
+				description = f"Use `{prefix}help <module>` to gain more information about that module 😁"
+			)
 
-				# iterating trough cogs, gathering descriptions
-				cogs_desc = ""
-				for cog in self.bot.cogs:
-					cogs_desc += f"`{cog}` {self.bot.cogs[cog].__doc__}\n"
+			# iterating trough cogs, gathering descriptions
+			cogs_desc = ""
+			for cog in self.bot.cogs:
+				cogs_desc += f"`{cog}` {self.bot.cogs[cog].__doc__}\n"
 
-				# adding 'list' of cogs to embed
+			# adding 'list' of cogs to embed
+			embed.add_field(
+				name = "Modules", 
+				value = cogs_desc, 
+				inline = False
+			)
+
+			# integrating trough uncategorized commands
+			commands_desc = ""
+			for command in self.bot.walk_commands():
+				# if cog not in a cog
+				# listing command if cog name is None and command isn't hidden
+				if not command.cog_name and not command.hidden:
+					commands_desc += f"{command.name} - {command.help}\n"
+
+			# adding those commands to embed
+			if commands_desc:
 				embed.add_field(
-					name = "Modules", 
-					value = cogs_desc, 
+					name = "Not belonging to a module",
+					value = commands_desc, 
 					inline = False
 				)
+			embed.set_footer(text = f"{owner}")
 
-				# integrating trough uncategorized commands
-				commands_desc = ""
-				for command in self.bot.walk_commands():
-					# if cog not in a cog
-					# listing command if cog name is None and command isn't hidden
-					if not command.cog_name and not command.hidden:
-						commands_desc += f"{command.name} - {command.help}\n"
+		# block called when one cog-name is given
+		# trying to find matching cog and it's commands
+		elif len(input) == 1:
 
-				# adding those commands to embed
-				if commands_desc:
-					embed.add_field(
-						name = "Not belonging to a module",
-						value = commands_desc, 
-						inline = False
-					)
-				embed.set_footer(text = f"{owner}")
+			# iterating trough cogs
+			for cog in self.bot.cogs:
+				# check if cog is the matching one
+				if cog.lower() == input[0].lower():
 
-			# block called when one cog-name is given
-			# trying to find matching cog and it's commands
-			elif len(input) == 1:
-
-				# iterating trough cogs
-				for cog in self.bot.cogs:
-					# check if cog is the matching one
-					if cog.lower() == input[0].lower():
-
-						# making title - getting description from doc-string below class
-						embed = discord.Embed(
-							title = f"{cog} - Commands", 
-							description = self.bot.cogs[cog].__doc__,
-							color=discord.Color.green()
-						)
-
-						# getting commands from cog
-						for command in self.bot.get_cog(cog).get_commands():
-							# if cog is not hidden
-							if not command.hidden:
-								embed.add_field(
-									name = f"`{prefix}{command.name}`", 
-									value = command.help, 
-									inline = False
-								)
-						# found cog - breaking loop
-						break
-
-				# if input not found
-				# yes, for-loops have an else statement, it's called when no 'break' was issued
-				else:
+					# making title - getting description from doc-string below class
 					embed = discord.Embed(
-						title="What's that?!",
-						description = f"I've never heard from a module called `{input[0]}` before :scream:",
-						color=discord.Color.orange()
+						title = f"{cog} - Commands", 
+						description = self.bot.cogs[cog].__doc__,
+						color=discord.Color.green()
 					)
 
-			# too many cogs requested - only one at a time allowed
-			elif len(input) > 1:
-				embed = discord.Embed(
-					title = "That's too much.",
-					description = "Please request only one module at once :sweat_smile:",
-					color = discord.Color.orange()
-				)
+					# getting commands from cog
+					for command in self.bot.get_cog(cog).get_commands():
+						# if cog is not hidden
+						if not command.hidden:
+							embed.add_field(
+								name = f"`{prefix}{command.name}`", 
+								value = command.help, 
+								inline = False
+							)
+					# found cog - breaking loop
+					break
 
+			# if input not found
+			# yes, for-loops have an else statement, it's called when no 'break' was issued
 			else:
 				embed = discord.Embed(
-					title = "It's a magical place.",
-					description = "I don't know how you got here. But I didn't see this coming at all.\n"
-									"Would you please be so kind to report that issue to me {}\n".format(owner)
-									"Thank you! ~ Lucifer Chase",
-					color = discord.Color.red())
+					title="What's that?!",
+					description = f"I've never heard from a module called `{input[0]}` before :scream:",
+					color=discord.Color.orange()
+				)
 
-			# sending reply embed using our own function defined above
-			await ctx.send(embed)
+		# too many cogs requested - only one at a time allowed
+		elif len(input) > 1:
+			embed = discord.Embed(
+				title = "That's too much.",
+				description = "Please request only one module at once :sweat_smile:",
+				color = discord.Color.orange()
+			)
+
+		else:
+			embed = discord.Embed(
+				title = "It's a magical place.",
+				description = "I don't know how you got here. But I didn't see this coming at all.\n"
+								"Would you please be so kind to report that issue to me {}\n".format(owner)
+								"Thank you! ~ Lucifer Chase",
+				color = discord.Color.red())
+
+		# sending reply embed using our own function defined above
+		await ctx.send(embed)
