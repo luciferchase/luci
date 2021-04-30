080186a51a9005ad3117031285eebdcb7c999692
Lucifer Chase <luciferchase@noreply.github.com>
2021-04-30 18:21:47 +0530

    Minor fix

--- bot/main.py
+++ bot/main.py
@@ -28,13 +28,10 @@ from cogs.photo import photo
 # Get Members intent
 intents = discord.Intents.all()
 
-# Configure help menu
-menu = DefaultMenu(color = 0xf34949, sort_commands = True)
-
 # Configure the bot
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = commands.Bot(command_prefix = ["luci ", "Luci "], case_insensitive = True, 
-	intents = intents, help_command = PrettyHelp(menu = menu))
+	intents = intents, help_command = PrettyHelp(color = 0xf34949, sort_commands = True))
 
 logging.basicConfig(level = logging.WARNING)
 
