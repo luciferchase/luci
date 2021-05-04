cd0924f75f1b23d7ab3d002cca47f4f1936be9cb
Lucifer Chase <udit2702@gmail.com>
2021-05-04 11:03:25 +0530

    Minor fix

--- bot/cogs/core/core.py
+++ bot/cogs/core/core.py
@@ -82,13 +82,9 @@ class Core(commands.Cog):
 		# Create a dm with me
 		luci = self.bot.get_user(707557256220115035)
 		dm_channel = await luci.create_dm()
-		print(message.guild)
-		print(message.guild is None)
-		print(message.author.bot)
-		print(message.author)
-
+		
 		# Forward all messages to me if the message is not from a guils, or by a bot or by me
-		if (message.guild is None or message.author.bot == False or message.author != luci):
+		if (message.guild is None or not message.author.bot or message.author != luci):
 			
 			embed = discord.Embed(title = "Direct Message", description = message.content, color = 0x00FFFF)
 			embed.set_author(name = message.author.name, icon_url = message.author.avatar_url)
