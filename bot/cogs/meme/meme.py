07389dfc880d1b552ef0647203dfef7eaf0a4c1c
Lucifer Chase <udit2702@gmail.com>
2021-05-05 10:05:59 +0530

    Moved from requests to aiohttp

--- bot/cogs/meme/meme.py
+++ bot/cogs/meme/meme.py
@@ -71,41 +71,41 @@ class Meme(commands.Cog):
 		if (count > 50):
 			await ctx.send("Bruh! Get a life bro. Can't request more than 50 memes at a time 🤦‍♂️")
 
-		async with self.session.get(self.meme_api + str(count)) as response:
+		async with self.session.get(self.meme_api + endpoint + str(count)) as response:
+			data = await response.json()
+			data = data["memes"]
+
 			# If the query was successfull
 			if (response.status == 200):
-				data = await response.json()
-				data = data["memes"]
-
 				for meme in data:
 					embed = discord.Embed(
 						color = 0x06f9f5,							# Blue-ish
-						title = data["title"],
-						url = data["postLink"]
+						title = meme["title"],
+						url = meme["postLink"]
 					)
-					embed.set_image(url = data["url"])
-					embed.set_footer(text = f'👍 {data["ups"]}')
+					embed.set_image(url = meme["url"])
+					embed.set_footer(text = f'👍 {meme["ups"]}')
 					await ctx.send(embed = embed)
 
-				else:
-					status_code = data["code"]
-					status_message = data["message"]
+			else:
+				status_code = data["code"]
+				status_message = data["message"]
 
-					# Else send a random dog photo
-					async with self.session.get(self.dog_api) as response:
-						log.error(f'Status Code: {status_code}')
-						log.error(f'Error: {status_message}')
+				# Else send a random dog photo
+				async with self.session.get(self.dog_api) as response:
+					log.error(f'Status Code: {status_code}')
+					log.error(f'Error: {status_message}')
 
-						data = await response.json()
-						data = data[0]
+					data = await response.json()
+					data = data[0]
 
-						embed = discord.Embed(
-							title = "Bruh...",
-							color = 0xea1010						# Red
-						)
-						embed.add_field(
-							name = status_message,
-							value = "Try again maybe? Anyway here is a cute doggo ❤"
-						)
-						embed.set_image(url = data["url"])
-						await ctx.send(embed = embed)
+					embed = discord.Embed(
+						title = "Bruh...",
+						color = 0xea1010						# Red
+					)
+					embed.add_field(
+						name = status_message,
+						value = "Try again maybe? Anyway here is a cute doggo ❤"
+					)
+					embed.set_image(url = data["url"])
+					await ctx.send(embed = embed)
