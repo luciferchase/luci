312db54cbd6c3f0df011382b6ba0d6b490ca08b6
Lucifer Chase <udit2702@gmail.com>
2021-05-06 11:53:38 +0530

    Fix error

--- bot/cogs/core/core.py
+++ bot/cogs/core/core.py
@@ -110,46 +110,44 @@ class Core(commands.Cog):
 			await dm_channel.send(embed = embed)
 			await message.channel.send(f"Message sent to {luci.name}")
 
-		# Check if the message is pinging an AFK member or an AFK member has returned
-		if (message.guild is not None and not message.author.bot):
-			# First create a table if no afk table is present
-			query = """CREATE TABLE IF NOT EXITS afk(
-					member_id 	BIGINT 		NOT NULL 	PRIMARY_KEY,
-					message		TEXT,
-					last_seen	TEXT		NOT NULL,
-					guild_id	BIGINT		NOT NULL)"""
-			self.cursor.execute(query)
-			self.dbcon.commit()
+		# First create a table if no afk table is present
+		query = """CREATE TABLE IF NOT EXITS afk(
+				member_id 	BIGINT 		NOT NULL 	PRIMARY_KEY,
+				message		TEXT,
+				last_seen	TEXT		NOT NULL,
+				guild_id	BIGINT		NOT NULL)"""
+		self.cursor.execute(query)
+		self.dbcon.commit()
 
-			# Fetch all AFK members
-			self.cursor.execute(f"""SELECT * FROM afk WHERE guild_id = {message.guild.id}""")
-			data = self.cursor.fetchall()
+		# Fetch all AFK members
+		self.cursor.execute(f"""SELECT * FROM afk WHERE guild_id = {message.guild.id}""")
+		data = self.cursor.fetchall()
 
-			for index in data:
-				afk_member = self.bot.get_user(data[index][0])
+		for index in data:
+			afk_member = self.bot.get_user(data[index][0])
 
-				# If an AFK member is pinged
-				if (member.mention in message):
-					await ctx.send(f"ping:839468910734606356 :: {message.author.mention}, **{afk_member.nick}** is \
-						currently AFK. [Last seen {data[index][2]}]")
+			# If an AFK member is pinged
+			if (member.mention in message):
+				await ctx.send(f"ping:839468910734606356 :: {message.author.mention}, **{afk_member.nick}** is \
+					currently AFK. [Last seen {data[index][2]}]")
 
-					# Send a reason if present
-					if (data[index][1] != ""):
-						await ctx.send(f"**Reason:** {data[index][1]}")
+				# Send a reason if present
+				if (data[index][1] != ""):
+					await ctx.send(f"**Reason:** {data[index][1]}")
 
-				# If an AFK member sends a message
-				if (message.author == afk_member):
-					# Remove from database
-					self.cursor.execute(f"""DELETE FROM afk WHERE member_id = {data[index][0]}""")
-					self.dbcon.commit()
+			# If an AFK member sends a message
+			if (message.author == afk_member):
+				# Remove from database
+				self.cursor.execute(f"""DELETE FROM afk WHERE member_id = {data[index][0]}""")
+				self.dbcon.commit()
 
-					# Change nickname
-					await afk_member.edit(nick = afk_member.nick[6:])
+				# Change nickname
+				await afk_member.edit(nick = afk_member.nick[6:])
 
-					# Get the nacho emoji
-					nacho = get(self.bot.get_all_emojis(), name = "nacho")
-					await ctx.send(f"blobwave:839737122633023498 :: Welcome back, {afk_member.mention}! \
-						I've removed your AFK status. Enjoy {nacho}")
+				# Get the nacho emoji
+				nacho = get(self.bot.get_all_emojis(), name = "nacho")
+				await ctx.send(f"blobwave:839737122633023498 :: Welcome back, {afk_member.mention}! \
+					I've removed your AFK status. Enjoy {nacho}")
 
 
 	# Add last 5 deleted message to database
