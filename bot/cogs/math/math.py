f7f575b159ff448749eff9118ec0b31b6522f097
Lucifer Chase <udit2702@gmail.com>
2021-05-08 07:32:14 +0530

    added bunch of commands using the newton api

--- bot/cogs/math/math.py
+++ bot/cogs/math/math.py
@@ -3,9 +3,11 @@ from discord.ext import commands
 from TagScriptEngine import Interpreter, adapter, block
 
 import aiohttp
+import aionewton
 import json
 import logging
 import time
+from urllib import parse
 
 
 class Math(commands.Cog):
@@ -72,8 +74,7 @@ class Math(commands.Cog):
 
 	@commands.command(aliases=["calc"])
 	async def calculate(self, ctx, *, query):
-		""" Faster but sometimes does not work.
-		"""
+		""" Faster but sometimes does not work."""
 		
 		query = query.replace(",", "")
 		engine_input = "{m:" + query + "}"
@@ -88,4 +89,125 @@ class Math(commands.Cog):
 			description = f"Output: `{output_string}`",
 		)
 		embed.set_footer(text = f"Calculated in {round((end - start) * 1000, 3)} ms")
+		await ctx.send(embed = embed)
+
+
+	async def send_embed(self, expression, answer):
+		embed = discord.Embed(
+			color = 0xf34949,									# Red					
+			title = result
+		)
+		embed.add_field(
+			name = "Your Input:",
+			value = f'`{expression}`',
+			inline = True
+		)
+		embed.add_field(
+			name = "Answer:",
+			value = f"`{answer}`",
+			inline = True
+		)
+		embed.set_footer(text = f"Calculated in {round((end - start) * 1000, 3)} ms")
+		return embed
+
+	
+	@commands.command(aliases = ["factor"])
+	async def factorise(self, ctx, *expression):
+		"""Factorise a polynomial equation
+		Usage: `luci factorise x^2 + 2x`"""
+		expression = "".join(expression)
+
+		loading = await ctx.send("Calculating...")
+		await ctx.trigger_typing()
+
+		answer = await aionewton.factor(expression)
+		embed = self.send_embed(expression = expression, answer = answer)
+
+		# First delete the calculating message
+		await loading.delete()
+		await ctx.send(embed = embed)
+
+
+	@commands.command(aliases = ["derivation", "differentiate"])
+	async def derive(self, ctx, *expression):
+		"""Differentiate a polynomial
+		Usage: `luci derive x^2 + 2x`"""
+		expression = "".join(expression)
+
+		loading = await ctx.send("Calculating...")
+		await ctx.trigger_typing()
+
+		answer = await aionewton.derive(expression)
+		embed = self.send_embed(expression = expression, answer = answer)
+
+		# First delete the calculating message
+		await loading.delete()
+		await ctx.send(embed = embed)
+
+
+	@commands.command(aliases = ["integration"])
+	async def integrate(self, ctx, *expression):
+		"""Integrate a polynomial
+		Usage: `luci integrate x^2 + 2x`"""
+		expression = "".join(expression)
+
+		loading = await ctx.send("Calculating...")
+		await ctx.trigger_typing()
+
+		answer = await aionewton.integrate(expression)
+		embed = self.send_embed(expression = expression, answer = answer)
+
+		# First delete the calculating message
+		await loading.delete()
+		await ctx.send(embed = embed)
+
+
+	@commands.command(aliases = ["solution", "zeroes", "roots"])
+	async def solve(self, ctx, *expression):
+		"""Find roots of a polynomial equation
+		Usage: `luci roots x^2 + 2x`"""
+		expression = "".join(expression)
+
+		loading = await ctx.send("Calculating...")
+		await ctx.trigger_typing()
+
+		answer = await aionewton.zeroes(expression)
+		embed = self.send_embed(expression = expression, answer = answer)
+
+		# First delete the calculating message
+		await loading.delete()
+		await ctx.send(embed = embed)
+
+
+	@commands.command()
+	async def tangent(self, ctx, *expression):
+		"""Find tangent of a curve at a point [Eg: `luci tangent 2|x^3`. See `luci help tangent`]
+		Usage: `luci tangent 2|x^3` # Here 2 is the point where tangent is to be find"""
+		expression = "".join(expression)
+
+		loading = await ctx.send("Calculating...")
+		await ctx.trigger_typing()
+
+		answer = await aionewton.tangent(expression)
+		embed = self.send_embed(expression = expression, answer = answer)
+
+		# First delete the calculating message
+		await loading.delete()
+		await ctx.send(embed = embed)
+
+
+	@commands.command(name = "area", aliases = ["area under the curve", "definite integration"])
+	async def definite_integral(self, ctx, *expression):
+		"""Do definite integration on a polynomial [Eg: `luci area 2:4|x^3`. See `luci help area`]
+		Usage: `luci area 2:4|x^3` # Here area is to be calulated from 2 to 4"""
+		expression = "".join(expression)
+
+		loading = await ctx.send("Calculating...")
+		await ctx.trigger_typing()
+
+		answer = await aionewton.area(expression)
+		embed = self.send_embed(expression = expression, answer = answer)
+
+		# First delete the calculating message
+		await loading.delete()
 		await ctx.send(embed = embed)
