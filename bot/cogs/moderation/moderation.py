ce98e8e63c3ecd340a88948a2789e82d8844ce2e
Lucifer Chase <udit2702@gmail.com>
2021-05-26 22:33:36 +0530

    Test moderation commands

--- bot/cogs/moderation/moderation.py
+++ bot/cogs/moderation/moderation.py
@@ -1,28 +1,322 @@
 import discord
 from discord.ext import commands
+from urllib.parse import urlparse
+import datetime
+import asyncio
+import random
+import pip
+import os
+import io
+import json
 
-class Moderation(commands.Cog):
+
+class Mod(commands.Cog):
     """Moderation commands for admins"""
+
     def __init__(self, bot):
         self.bot = bot
 
-    #The below code bans player.
+    async def format_mod_embed(self, ctx, user, success, method, duration = None, location = None):
+        """Helper function to format an embed to prevent extra code"""
+        
+        embed = discord.Embed(timestamp = ctx.message.created_at)
+        embed.set_author(name = method.title(), icon_url = user.avatar_url)
+        embed.color = await ctx.get_dominant_color(user.avatar_url)
+        embed.set_footer(text = f"User ID: {user.id}")
+        
+        if success:
+            if method == "ban" or method == "hackban":
+                embed.description = f"{user} was just {method}ned."
+            elif method == "unmute":
+                embed.description = f"{user} was just {method}d."
+            elif method == "mute":
+                embed.description = f"{user} was just {method}d for {duration}."
+            elif method == "channel-lockdown" or method == "server-lockdown":
+                embed.description = f"`{location.name}` is now in lockdown mode!"
+            else:
+                embed.description = f"{user} was just {method}ed."
+        else:
+            if method == "lockdown" or "channel-lockdown":
+                embed.description = f"You do not have the permissions to {method} `{location.name}`."
+            else:
+                embed.description = f"You do not have the permissions to {method} {user.name}."
+        
+        return embed
+
     @commands.command()
-    @commands.has_permissions(ban_members = True)
-    async def ban(ctx, member : discord.Member, *, reason = None):
-        await member.ban(reason = reason)
+    async def kick(self, ctx, member: discord.Member, *, reason = None):
+        """Kick someone from the server."""
 
-    #The below code unbans player.
+        try:
+            await ctx.guild.kick(member, reason = reason)
+        except:
+            success = False
+        else:
+            success = True
+
+        embed = await self.format_mod_embed(ctx, member, success, "kick")
+        await ctx.send(embed = embed)
+
     @commands.command()
-    @commands.has_permissions(administrator = True)
-    async def unban(ctx, *, member):
-        banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split("#")
+    async def ban(self, ctx, member: discord.Member, *, reason = None):
+        """Ban someone from the server."""
 
-        for ban_entry in banned_users:
-            user = ban_entry.user
+        try:
+            await ctx.guild.ban(member, reason = reason)
+        except:
+            success = False
+        else:
+            success = True
 
-            if (user.name, user.discriminator) == (member_name, member_discriminator):
-                await ctx.guild.unban(user)
-                await ctx.send(f'Unbanned {user.mention}')
-                return
+        embed = await self.format_mod_embed(ctx, member, success, "ban")
+        await ctx.send(embed = embed)
+
+    @commands.command()
+    async def unban(self, ctx, name_or_id, *, reason = None):
+        """Unban someone from the server."""
+
+        ban = await ctx.get_ban(name_or_id)
+
+        try:
+            await ctx.guild.unban(ban.user, reason = reason)
+        except:
+            success = False
+        else:
+            success = True
+        
+        embed = await self.format_mod_embed(ctx, ban.user, success, "unban")
+        await ctx.send(embed = embed)
+
+    @commands.command()
+    async def purge(self, ctx, limit : int, member: discord.Member = None):
+        """Clean a number of messages"""
+
+        if member is None:
+            await ctx.purge(limit = limit + 1)
+        else:
+            async for message in ctx.channel.history(limit = limit + 1):
+                if message.author is member:
+                    await message.delete()
+
+    @commands.command()
+    async def clean(self, ctx, quantity: int):
+        """ Clean a number of your own messages
+        Usage: {prefix}clean 5 """
+
+        if quantity <= 15:
+            total = quantity + 1
+            async for message in ctx.channel.history(limit = total):
+                if message.author == ctx.author:
+                    await message.delete()
+                    await asyncio.sleep(3.0)
+        else:
+            async for message in ctx.channel.history(limit = 6):
+                if message.author == ctx.author:
+                    await message.delete()
+                    await asyncio.sleep(3.0)
+
+    @commands.command()
+    async def bans(self, ctx):
+        """See a list of banned users in the guild"""
+
+        try:
+            bans = await ctx.guild.bans()
+        except:
+            return await ctx.send("You dont have the perms to see bans.")
+
+        embed = discord.Embed(title = f"List of Banned Members ({len(bans)}):")
+        embed.description = ", ".join([str(b.user) for b in bans])
+        embed.color = await ctx.get_dominant_color(ctx.guild.icon_url)
+
+        await ctx.send(embed = embed)
+
+    @commands.command()
+    async def baninfo(self, ctx, *, name_or_id):
+        """Check the reason of a ban from the audit logs."""
+
+        ban = await ctx.get_ban(name_or_id)
+
+        embed = discord.Embed()
+        embed.color = await ctx.get_dominant_color(ban.user.avatar_url)
+        embed.set_author(name = str(ban.user), icon_url = ban.user.avatar_url)
+        embed.add_field(name="Reason", value = ban.reason or "None")
+        embed.set_thumbnail(url = ban.user.avatar_url)
+        embed.set_footer(text = f"User ID: {ban.user.id}")
+
+        await ctx.send(embed = embed)
+
+    @commands.command()
+    async def addrole(self, ctx, member: discord.Member, *, rolename: str):
+        """Add a role to someone else."""
+
+        role = discord.utils.find(lambda m: rolename.lower() in m.name.lower(), ctx.message.guild.roles)
+
+        if not role:
+            return await ctx.send("That role does not exist.")
+        try:
+            await member.add_roles(role)
+            await ctx.send(f"Added: `{role.name}`")
+        except:
+            await ctx.send("I don't have the perms to add that role.")
+
+
+    @commands.command()
+    async def removerole(self, ctx, member: discord.Member, *, rolename: str):
+        """Remove a role from someone else."""
+
+        role = discord.utils.find(lambda m: rolename.lower() in m.name.lower(), ctx.message.guild.roles)
+        if not role:
+            return await ctx.send("That role does not exist.")
+        try:
+            await member.remove_roles(role)
+            await ctx.send(f"Removed: `{role.name}`")
+        except:
+            await ctx.send("I don't have the perms to add that role.")
+
+    @commands.command()
+    async def hackban(self, ctx, userid, *, reason = None):
+        """Ban someone not in the server"""
+
+        try:
+            userid = int(userid)
+        except:
+            await ctx.send("Invalid ID!")
+        
+        try:
+            await ctx.guild.ban(discord.Object(userid), reason = reason)
+        except:
+            success = False
+        else:
+            success = True
+
+        if success:
+            async for entry in ctx.guild.audit_logs(limit = 1, user = ctx.guild.me, action = discord.AuditLogAction.ban):
+                embed = await self.format_mod_embed(ctx, entry.target, success, "hackban")
+        else:
+            embed = await self.format_mod_embed(ctx, userid, success, "hackban")
+        await ctx.send(embed = embed)
+
+    @commands.command()
+    async def mute(self, ctx, member:discord.Member, duration, *, reason = None):
+        """Denies someone from chatting in all text channels and \
+        talking in voice channels for a specified duration"""
+
+        unit = duration[-1]
+        
+        if unit == "s":
+            time = int(duration[:-1])
+            longunit = "seconds"
+        elif unit == "m":
+            time = int(duration[:-1]) * 60
+            longunit = "minutes"
+        elif unit == "h":
+            time = int(duration[:-1]) * 60 * 60
+            longunit = "hours"
+        else:
+            await ctx.send("Invalid Unit! Use `s`, `m`, or `h`.")
+            return
+
+        progress = await ctx.send("Muting user!")
+        
+        try:
+            for channel in ctx.guild.text_channels:
+                await channel.set_permissions(
+                    member, 
+                    overwrite = discord.PermissionOverwrite(send_messages = False), 
+                    reason = reason
+                )
+
+            for channel in ctx.guild.voice_channels:
+                await channel.set_permissions(
+                    member, 
+                    overwrite = discord.PermissionOverwrite(speak = False), 
+                    reason = reason
+                )
+        except:
+            success = False
+        else:
+            success = True
+            
+        progress.delete()
+
+        embed = await self.format_mod_embed(ctx, member, success, "mute", f"{str(duration[:-1])} {longunit}")
+        await ctx.send(embed = embed)
+        
+        await asyncio.sleep(time)
+        
+        try:
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(member, overwrite = None, reason = reason)
+        except:
+            pass
+        
+    @commands.command()
+    async def unmute(self, ctx, member:discord.Member, *, reason = None):
+        """Removes channel overrides for specified member"""
+
+        progress = await ctx.send("Unmuting user!")
+        
+        try:
+            for channel in ctx.message.guild.channels:
+                await channel.set_permissions(member, overwrite = None, reason = reason)
+        except:
+            success = False
+        else:
+            success = True
+
+        progress.delete()
+            
+        embed = await self.format_mod_embed(ctx, member, success, "unmute")
+        await ctx.send(embed = embed)
+
+    @commands.group(invoke_without_command = True)
+    async def lockdown(self, ctx):
+        """Server/Channel lockdown"""
+        pass
+
+    @lockdown.command(aliases=["channel"])
+    async def chan(self, ctx, channel: discord.TextChannel = None, *, reason = None):
+        if channel is None: 
+            channel = ctx.channel
+        
+        try:
+            await channel.set_permissions(
+                ctx.guild.default_role, 
+                overwrite = discord.PermissionOverwrite(send_messages = False), 
+                reason = reason
+            )
+        except:
+            success = False
+        else:
+            success = True
+        
+        embed = await self.format_mod_embed(ctx, ctx.author, success, "channel-lockdown", 0, channel)
+        await ctx.send(embed = embed)
+    
+    @lockdown.command()
+    async def server(self, ctx, server: discord.Guild = None, *, reason = None):
+        if server is None: 
+            server = ctx.guild
+        
+        progress = await ctx.send(f"Locking down {server.name}")
+        
+        try:
+            for channel in server.channels:
+                await channel.set_permissions(
+                    ctx.guild.default_role, 
+                    overwrite = discord.PermissionOverwrite(send_messages = False), 
+                    reason = reason
+                )
+        except:
+            success = False
+        else:
+            success = True
+        
+        progress.delete()
+        
+        embed = await self.format_mod_embed(ctx, ctx.author, success, "server-lockdown", 0, server)
+        await ctx.send(embed = embed)
+
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
