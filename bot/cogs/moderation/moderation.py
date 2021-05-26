203a5e40e3519fa9a07a2bd13f13f7504be0ec39
Lucifer Chase <udit2702@gmail.com>
2021-05-26 21:22:52 +0530

    Test moderation commands

--- bot/cogs/moderation/moderation.py
+++ bot/cogs/moderation/moderation.py
@@ -10,7 +10,7 @@ class Moderation(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(ctx, member : discord.Member, *, reason = None):
-        await guild.ban(member, reason = reason)
+        await member.ban(reason = reason)
 
     #The below code unbans player.
     @commands.command()
