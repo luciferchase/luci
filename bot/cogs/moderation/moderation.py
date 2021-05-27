ace0d3bbea1a9c328b8dbdf8efec41dcdfc682bb
Lucifer Chase <udit2702@gmail.com>
2021-05-27 08:00:46 +0530

    Fix errors

--- bot/cogs/moderation/moderation.py
+++ bot/cogs/moderation/moderation.py
@@ -4,6 +4,27 @@ from discord.ext import commands
 import asyncio
 
 
+class GetFetchUser(UserConverter):
+    async def convert(self, ctx, user: str) -> User:
+        """
+        :param user: str argument coming from Discord.
+        Tries to get user from cache and if fails tries to fetch user from Discord.
+        If both fail raises BadArgument
+        """
+        try:
+            return await super().convert(ctx, user)
+        except BadArgument:
+            try:
+                user_id = int(user)
+            except ValueError:
+                raise BadArgument("Can't convert to neither user nor snowflake.")
+            else:
+                try:
+                    return await ctx.bot.fetch_user(user_id)
+                except HTTPException as e:
+                    raise BadArgument(e)
+
+
 class Mod(commands.Cog):
     """Moderation commands for admins"""
 
