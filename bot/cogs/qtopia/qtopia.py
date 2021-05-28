a54eb94a62e058fccdf3790105cdfe21af41cb8a
Lucifer Chase <udit2702@gmail.com>
2021-05-28 16:27:38 +0530

    Add bunch of new commands

--- bot/cogs/qtopia/qtopia.py
+++ bot/cogs/qtopia/qtopia.py
@@ -14,7 +14,7 @@ class Qtopia(commands.Cog):
         if isinstance(ctx.channel, discord.channel.DMChannel):
             message = " ".join(message)
             channel = await self.bot.fetch_channel(739150769722228806)
-            await ctx.send(message)
+            await channel.send(message)
             await ctx.send(f"{ctx.author.name} message sent to #vent-1")
 
     @commands.command()
@@ -23,7 +23,7 @@ class Qtopia(commands.Cog):
         if isinstance(ctx.channel, discord.channel.DMChannel):
             message = " ".join(message)
             channel = await self.bot.fetch_channel(793407631066005554)
-            await ctx.send(message)
+            await channel.send(message)
             await ctx.send(f"{ctx.author.name} message sent to #vent-2")
 
     @commands.command()
@@ -32,5 +32,5 @@ class Qtopia(commands.Cog):
         if isinstance(ctx.channel, discord.channel.DMChannel):
             message = " ".join(message)
             channel = await self.bot.fetch_channel(639902815849938975)
-            await ctx.send(message)
+            await channel.send(message)
             await ctx.send(f"{ctx.author.name} message sent to #q-and-a")
