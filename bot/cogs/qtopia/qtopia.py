f2677dfdb57d686e68ba17c24f61349b1b5a92c4
Lucifer Chase <udit2702@gmail.com>
2021-05-28 15:59:33 +0530

    Add bunch of new commands

--- bot/cogs/qtopia/qtopia.py
+++ bot/cogs/qtopia/qtopia.py
@@ -10,22 +10,25 @@ class Qtopia(commands.Cog):
         self.guild = self.bot.get_guild(587139618999369739)
 
     @commands.command()
-    async def vent1(self, *, message):
+    async def vent1(self, *message):
         if isinstance(ctx.channel, discord.channel.DMChannel) and message.author in self.guild.members:
+            message = " ".join(message)
             channel = self.bot.get_channel(739150769722228806)
             await ctx.send(message)
             await ctx.send(f"{message.author.name} message sent to #vent-1")
 
     @commands.command()
-    async def vent2(self, *, message):
+    async def vent2(self, *message):
         if isinstance(ctx.channel, discord.channel.DMChannel) and message.author in self.guild.members:
+            message = " ".join(message)
             channel = self.bot.get_channel(793407631066005554)
             await ctx.send(message)
             await ctx.send(f"{message.author.name} message sent to #vent-2")
 
     @commands.command()
-    async def ask(self, *, message):
+    async def ask(self, *message):
         if isinstance(ctx.channel, discord.channel.DMChannel) and message.author in self.guild.members:
+            message = " ".join(message)
             channel = self.bot.get_channel(639902815849938975)
             await ctx.send(message)
             await ctx.send(f"{message.author.name} message sent to #q-and-a")
