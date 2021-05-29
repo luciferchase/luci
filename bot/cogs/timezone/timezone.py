a09ab2b9a221e984053a7c54736e4bb106b33c14
Lucifer Chase <udit2702@gmail.com>
2021-05-29 06:56:02 +0530

    Test Timezone cog

--- bot/cogs/timezone/timezone.py
+++ bot/cogs/timezone/timezone.py
@@ -26,14 +26,14 @@ class Timezone(commands.Cog):
         else:
             await ctx.send("Uh oh! No country found 👀")
             await ctx.send("You can check list of accepted timezones using `luci timezones [continent name]`")
+            break
 
-        if not time:
-            # Current time in UTC
-            now_utc = datetime.now(pytz.timezone("UTC"))
+        # Current time in UTC
+        now_utc = datetime.now(pytz.timezone("UTC"))
 
-            # Convert it to appropriate timezone
-            different_tz = now_utc.astimezone(pytz.timezone(country))
-            await ctx.send("```css\n{}: {}```".format(country, different_tz.strftime('%H:%M')))
+        # Convert it to appropriate timezone
+        different_tz = now_utc.astimezone(pytz.timezone(country))
+        await ctx.send("```css\n{}: {}```".format(country, different_tz.strftime('%H:%M')))
 
     @commands.command()
     async def timezones(self, ctx, *continent):
@@ -43,5 +43,7 @@ class Timezone(commands.Cog):
         for tz in list(pytz.all_timezones):
             if (continent.title() in tz):
                 tz_list.append(tz)
+        else:
+            await ctx.send(f"Bruh! Are you sure {" ".join(continent.split("_")).title()} is a continent? 🤦‍♂️")
 
         await ctx.send("```css\n{}```".format('\n'.join(tz_list)))
