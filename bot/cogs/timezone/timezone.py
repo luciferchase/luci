80389366cc689c3023f5093d846853bbfea590da
Lucifer Chase <udit2702@gmail.com>
2021-05-29 06:27:19 +0530

    Test Timezone cog

--- bot/cogs/timezone/timezone.py
+++ bot/cogs/timezone/timezone.py
@@ -8,14 +8,14 @@ import pytz
 class Timezone(commands.Cog):
     """Convert local time to different timezones"""
 
-    @commands.command(aliases = ["tz", "timezone"])
+    @commands.command(aliases = ["tz"])
     async def time(self, ctx, country, *, time = None):
         """Example: luci tz america 12:56"""
 
         if (country.lower() == "usa"):
             country = "america"
 
-        list_of_timezones = pytz.all_timezones
+        list_of_timezones = list(pytz.all_timezones)
         
         for i in range(len(list_of_timezones)):
             if (country.title() in list_of_timezones[i]):
@@ -23,8 +23,22 @@ class Timezone(commands.Cog):
                 break
         else:
             await ctx.send("Uh oh! No country found 👀")
-            await ctx.send("You can check list of accepted countries [here](http://en.wikipedia.org/wiki/List_of_tz_database_time_zones)")
+            await ctx.send("You can check list of accepted timezones using `luci timezones [continent name]`")
 
         if not time:
-            time = datetime.now(timezone(country)).strftime("%H:%M")
-            await ctx.send(time)
+            # Current time in UTC
+            now_utc = datetime.now(pytz.timezone("UTC"))
+
+            # Convert it to appropriate timezone
+            now_asia = now_utc.astimezone(pytz.timezone(country))
+            await ctx.send(f"```css\n{country}: {now_asia.strftime("%H:%M")}```")
+
+    @commands.command()
+    async def timezones(self, ctx, continent):
+        tz_list = []
+
+        for tz in list(pytz.all_timezones):
+            if (continent in tz):
+                tz_list.append(tz)
+
+        await ctx.send(f"```css\n{'\n'.join(tz_list)}```")
